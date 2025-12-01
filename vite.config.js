461da889018f22f40eb1e39eefe126c85cb31150
Mohamed Ali ZID <mohamedali@example.com>
2025-12-01 10:47:04 +0100

    Fix base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/devpack-website/',
+  base: './',
   plugins: [react()],
   css: {
     postcss: './postcss.config.js'  // Laissez comme ça, c'est correct
